649f0aa9689e93499ec2a99f3e736150067f9d27
HelenMallett <[helenkwmallett@googlemail.com]>
2016-07-01 15:47:45 +0100

    fixing gdp data functions

--- gdp_data/gdpreaddata.py
+++ gdp_data/gdpreaddata.py
@@ -57,11 +57,35 @@ def readgdp():
 
 # 
 
-# In[177]:
+# In[181]:
 
 #country='GBR'
     
 def gdpdata_for_country(country):
+    
+    row_number = 0
+    years=[]
+    data=[]
+    country=[]
+    country_code = []
+
+    with open('gdp-data-cleaned.csv',newline='') as csvfile:
+        gdpreader=csv.reader(csvfile,delimiter=',',quotechar='|')
+        for row in gdpreader:
+            data_row_list_num=[]
+            if row_number==0: # header row gives years in data
+                years=row[2:]
+            if row_number>0: # splitting rows into countrynames, countrycodes, and gdp data
+                data_row_list_str=row[2:]
+                data_row=[float(i) for i in data_row_list_str]
+                country_row=row[0]
+                country_code_row=row[1]
+                data.append(data_row)
+                country.append(country_row)
+                country_code.append(country_code_row)
+            row_number=row_number+1
+            
+    data_2=np.array(data)
     
     for i in range (0, len(country_code)):
         if country == country_code[i]:
@@ -72,9 +96,9 @@ def gdpdata_for_country(country):
 #print(data_for_country)
 
 
-# In[168]:
+# In[ ]:
 
-np.shape(data_2)
+
 
 
 # In[ ]:
